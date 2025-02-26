8d458820e83cd35eac36d0db8a31f55fcd282051
Olyad-dev <olayidgetinet@gmail.com>
2025-02-27 02:02:27 +0300

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    </>
-  );
+  return (  
+    <div className="App">
+      <Home/>
+    </div>
+  ); 
 }
 
 export default App;

f16b5b0867bbbdb43203d6e24adcd487ae4a1f9c
Olyad-dev <olayidgetinet@gmail.com>
2025-02-27 03:10:50 +0300

    axios and request

---
+++ src/Utils/Request.jsx
@@ -0,0 +1,15 @@
+const TheApiKey = process.env.REACT_APP_TheApiKey;
+
+const request = {
+  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${TheApiKey}&language=en-US`,
+  fetchPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${TheApiKey}&language=en-US&page=1`,
+  fetchOrigins: `https://api.themoviedb.org/3/discover/tv?api_key=${TheApiKey}&with_networks=213`,
+  fetchTopRatedMovies: `https://api.themoviedb.org/3/movie/top_rated?api_key=${TheApiKey}&language=en-US&page=1`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TheApiKey}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TheApiKey}&with_genres=35`,
+  fetchHorror: `https://api.themoviedb.org/3/search/movie?api_key=${TheApiKey}&language=en-US&query=horror&page=1&include_adult=false`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TheApiKey}&with_genres=10749`,
+  fetchDocumantries: `https://api.themoviedb.org/3/discover/movie?api_key=${TheApiKey}&with_genres=99`,
+};
+
+export default request;
